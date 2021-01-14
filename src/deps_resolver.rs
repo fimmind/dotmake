145c6d17c611e1ab62c4d31e60aee0ae434fa05f
fimmind <fimmind@mail.ru>
2021-01-14 12:41:49 +0300

    DepsResolver's structs implementation

--- src/deps_resolver.rs
+++ src/deps_resolver.rs
@@ -1,17 +1,34 @@
+use crate::deserializers::identifiers_set;
 use crate::identifier::Identifier;
+use std::collections::HashSet;
 use std::error::Error;
 
 #[derive(Debug, Deserialize)]
-pub struct DepsConf;
+pub struct DepsConf {
+    #[serde(default, deserialize_with = "identifiers_set")]
+    pub deps: HashSet<Identifier>,
 
-pub struct DepsResolver {}
+    #[serde(default, deserialize_with = "identifiers_set")]
+    pub post: HashSet<Identifier>,
 
-impl DepsResolver {
-    pub fn init(
-        roots: Vec<Identifier>,
-        get_deps_for: impl Fn(&Identifier) -> Result<&DepsConf, Box<dyn Error>>,
-    ) -> Self {
-        todo!("Dependecies resolving")
+    #[serde(default, deserialize_with = "identifiers_set")]
+    pub synonyms: HashSet<Identifier>,
+}
+
+pub struct DepsResolver<F> {
+    roots: Vec<Identifier>,
+    get_deps_for: F,
+}
+
+impl<F> DepsResolver<F>
+where
+    F: Fn(&Identifier) -> Result<&DepsConf, Box<dyn Error>>,
+{
+    pub fn init(roots: Vec<Identifier>, get_deps_for: F) -> Self {
+        DepsResolver {
+            roots,
+            get_deps_for,
+        }
     }
 
     pub fn try_resolve(&self) -> Result<Vec<&Identifier>, Box<dyn Error>> {
