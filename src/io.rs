f75d6e97ec218e27e651c59126b00a288824d864
fimmind <fimmind@mail.ru>
2021-01-24 15:04:54 +0300

    Prettify dotmake's messages

--- src/io.rs
+++ src/io.rs
@@ -1,15 +1,37 @@
-macro_rules! msg_fmt {
-    ($($fmt_args:expr),*) => {{
-        format!("[{}] {}", env!("CARGO_PKG_NAME"), format!($($fmt_args),+))
-    }};
-}
+use colored::*;
+use dialoguer::{Confirm, theme::ColorfulTheme};
+use std::fmt::{self, Display};
+use std::str;
+use crate::cli::OPTIONS;
 
 // OUTPUT
 // ------
-macro_rules! print_msg {
-    ($($fmt_args:expr),+) => {
-        eprintln!("{}", msg_fmt!($($fmt_args),+));
-    };
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum MessageType {
+    Info,
+    Warning,
+    Error,
+}
+
+impl Display for MessageType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MessageType::Info => write!(f, "{} ", "info:".bright_black()),
+            MessageType::Warning => write!(f, "{} ", "warning:".yellow().bold()),
+            MessageType::Error => write!(f, "{} ", "error:".red().bold()),
+        }
+    }
+}
+
+pub fn print_msg(msg: &str, msg_type: MessageType) {
+    eprint!("{}", msg_type);
+
+    let msg = textwrap::fill(msg, 80);
+    if msg.lines().take(2).count() > 1 {
+        eprint!("\n{}", textwrap::indent(&msg, "  "));
+    } else {
+        eprintln!("{}", msg);
+    }
 }
 
 #[macro_export]
@@ -32,38 +54,38 @@ macro_rules! exit_error_fn {
 
 #[macro_export]
 macro_rules! print_error {
-    ($($fmt_args:expr),+) => {
-        print_msg!("ERROR:   {}", format!($($fmt_args),+));
-    };
+    ($($fmt_args:expr),+) => {{
+        use $crate::io::*;
+        print_msg(&format!($($fmt_args),+), MessageType::Error);
+    }};
 }
 
 #[macro_export]
-macro_rules! print_warning {
-    ($($fmt_args:expr),+) => {
-        print_msg!("WARNING: {}", format!($($fmt_args),+));
-    };
+macro_rules! print_warn {
+    ($($fmt_args:expr),+) => {{
+        use $crate::io::*;
+        print_msg(&format!($($fmt_args),+, MessageType::Warning));
+    }};
 }
 
 #[macro_export]
 macro_rules! print_info {
-    ($($fmt_args:expr),+) => {
-        print_msg!("INFO:    {}", format!($($fmt_args),+));
-    };
+    ($($fmt_args:expr),+) => {{
+        use $crate::io::*;
+        print_msg(&format!($($fmt_args),+), MessageType::Info);
+    }};
 }
 
 // INPUT
 // -----
-#[macro_export]
-macro_rules! confirm {
-    ($default:expr; $($fmt_arg:expr),+) => {
-        if $crate::cli::OPTIONS.noconfirm() {
-            $default
-        } else {
-            dialoguer::Confirm::new()
-                .with_prompt(msg_fmt!($($fmt_arg),+))
-                .default($default)
-                .interact()
-                .unwrap_or_else(exit_error_fn!("Failed to perform input"))
-        }
-    };
+pub fn confirm(prompt: &str, default: bool) -> bool {
+    if OPTIONS.noconfirm() {
+        default
+    } else {
+        Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt(prompt)
+            .default(default)
+            .interact()
+            .unwrap_or_else(exit_error_fn!("Failed to perform input"))
+    }
 }
