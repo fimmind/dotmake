8d46fa246e9148bcf50e1faa59d579c493489131
fimmind <fimmind@mail.ru>
2021-01-25 19:19:46 +0300

    Minor code organization change

--- src/deps_resolver/mod.rs
+++ src/deps_resolver/mod.rs
@@ -5,6 +5,50 @@ use deps_graph::DepsGraph;
 use std::collections::HashSet;
 use std::error::Error;
 
+pub struct DepsResolver<'a> {
+    deps_graph: DepsGraph<&'a Identifier>,
+}
+
+impl<'a> DepsResolver<'a> {
+    pub fn init(
+        roots: impl IntoIterator<Item = &'a Identifier>,
+        get_deps_for: impl Fn(&'a Identifier) -> &'a DepsConf,
+    ) -> Self {
+        DepsResolver {
+            deps_graph: Self::build_deps_graph(roots, get_deps_for),
+        }
+    }
+
+    fn build_deps_graph(
+        roots: impl IntoIterator<Item = &'a Identifier>,
+        get_deps_for: impl Fn(&'a Identifier) -> &'a DepsConf,
+    ) -> DepsGraph<&'a Identifier> {
+        let mut visited = HashSet::new();
+        let mut stack = roots.into_iter().collect::<Vec<_>>();
+        let mut deps_graph = DepsGraph::init(stack.iter().map(|&i| i));
+        while let Some(node) = stack.pop() {
+            let deps_conf = get_deps_for(node);
+            for dep in deps_conf.deps() {
+                deps_graph.add_dep(node, dep);
+                if visited.insert(dep) {
+                    stack.push(dep);
+                }
+            }
+            for post in deps_conf.post_deps() {
+                deps_graph.add_dep(post, node);
+                if visited.insert(post) {
+                    stack.push(post)
+                }
+            }
+        }
+        deps_graph
+    }
+
+    pub fn try_resolve(self) -> Result<impl Iterator<Item = &'a Identifier>, Box<dyn Error>> {
+        Ok(self.deps_graph.into_iter()) // TODO: handle loops
+    }
+}
+
 #[derive(Debug)]
 pub struct DepsConf {
     deps: HashSet<Identifier>,
@@ -67,49 +111,5 @@ impl DepsConf {
     pub fn disjoin(&mut self, other: &Self) {
         self.remove_deps(&other.deps);
         self.remove_post_deps(&other.post_deps);
-    }
-}
-
-pub struct DepsResolver<'a> {
-    deps_graph: DepsGraph<&'a Identifier>,
-}
-
-impl<'a> DepsResolver<'a> {
-    pub fn init(
-        roots: impl IntoIterator<Item = &'a Identifier>,
-        get_deps_for: impl Fn(&'a Identifier) -> &'a DepsConf,
-    ) -> Self {
-        DepsResolver {
-            deps_graph: Self::build_deps_graph(roots, get_deps_for),
-        }
-    }
-
-    fn build_deps_graph(
-        roots: impl IntoIterator<Item = &'a Identifier>,
-        get_deps_for: impl Fn(&'a Identifier) -> &'a DepsConf,
-    ) -> DepsGraph<&'a Identifier> {
-        let mut visited = HashSet::new();
-        let mut stack = roots.into_iter().collect::<Vec<_>>();
-        let mut deps_graph = DepsGraph::init(stack.iter().map(|&i| i));
-        while let Some(node) = stack.pop() {
-            let deps_conf = get_deps_for(node);
-            for dep in deps_conf.deps() {
-                deps_graph.add_dep(node, dep);
-                if visited.insert(dep) {
-                    stack.push(dep);
-                }
-            }
-            for post in deps_conf.post_deps() {
-                deps_graph.add_dep(post, node);
-                if visited.insert(post) {
-                    stack.push(post)
-                }
-            }
-        }
-        deps_graph
-    }
-
-    pub fn try_resolve(self) -> Result<impl Iterator<Item = &'a Identifier>, Box<dyn Error>> {
-        Ok(self.deps_graph.into_iter()) // TODO: handle loops
     }
 }
