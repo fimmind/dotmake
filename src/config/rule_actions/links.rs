f80bcc5889c0621c408e3daa0e96fe902b2276ee
fimmind <fimmind@mail.ru>
2021-02-08 23:05:17 +0300

    Minor changes to `rule_actions::links` module

--- src/config/rule_actions/links.rs
+++ src/config/rule_actions/links.rs
@@ -1,8 +1,9 @@
+//! An action that creates soft links to actions dotfiles
+
 use super::{Action, RuleActionsConf};
 use crate::cli::OPTIONS;
-use crate::os::{
-    ensure_dir_exists, get_file_name, is_symlink, move_file, remove_file, symlink, OSError,
-};
+use crate::config::deserializers::List;
+use crate::os::{self, OSError};
 use std::collections::HashMap;
 use std::env::set_current_dir;
 use std::error::Error;
@@ -11,34 +12,36 @@ use std::path::{Path, PathBuf};
 #[derive(Debug, Deserialize)]
 #[serde(transparent)]
 pub struct Links {
-    links: HashMap<PathBuf, PathBuf>,
+    links: HashMap<PathBuf, List<PathBuf>>,
 }
 
 impl Action for Links {
     fn perform(&self, conf: &RuleActionsConf) -> Result<(), Box<dyn Error>> {
         set_current_dir(OPTIONS.dotfiles_dir())?;
-        for (source, dest) in &self.links {
-            if dest.exists() {
-                if is_symlink(dest)? {
-                    remove_file(dest)?;
-                } else {
-                    backup(dest, &conf.backup_dir)?;
+        for (source, dests) in &self.links {
+            for dest in dests.iter() {
+                if dest.exists() {
+                    if os::is_symlink(dest)? {
+                        os::remove_file(dest)?;
+                    } else {
+                        backup(dest, &conf.backup_dir)?;
+                    }
                 }
+                os::symlink(source, dest)?;
             }
-            symlink(source, dest)?;
         }
         Ok(())
     }
 }
 
 fn backup(file: &Path, backup_dir: &Path) -> Result<(), OSError> {
-    ensure_dir_exists(backup_dir)?;
+    os::ensure_dir_exists(backup_dir)?;
     let backup_file_path = get_backup_file_path(file, backup_dir)?;
-    move_file(file, &backup_file_path)
+    os::move_file(file, &backup_file_path)
 }
 
 fn get_backup_file_path(file: &Path, backup_dir: &Path) -> Result<PathBuf, OSError> {
-    let fname = get_file_name(&file)?;
+    let fname = os::get_file_name(&file)?;
     let mut backup_file_path = backup_dir.join(fname);
 
     let mut i = 1u128;
