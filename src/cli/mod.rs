bfe953f5e78f74ac4cf928bb9e8a3a9e4837bdf9
fimmind <fimmind@mail.ru>
2021-02-12 18:21:37 +0300

    Move `Options` to separate module

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,12 +1,12 @@
 //! CLI abstraction
 
+mod options;
 mod subcommand;
 
-use crate::os::{self, OSError};
 use lazy_static::lazy_static;
-use std::path::PathBuf;
 use structopt::StructOpt;
 
+use options::Options;
 use subcommand::Subcommand;
 
 lazy_static! {
@@ -28,49 +28,4 @@ pub struct Cli {
 
     #[structopt(subcommand)]
     subcommand: Subcommand,
-}
-
-#[derive(Debug, StructOpt)]
-pub struct Options {
-    /// Set a custom dotfiles directory
-    #[structopt(
-        short = "d",
-        long = "dotdir",
-        env = "DOTM_DOTFILES_DIR",
-        value_name = "DIR",
-        default_value = "./",
-        global = true
-    )]
-    dotfiles_dir: PathBuf,
-
-    /// Specify linux distribution id to use
-    #[structopt(short = "D", long = "distro", value_name = "ID", global = true)]
-    distro_id: Option<String>,
-
-    /// Use default values for confirmation dialogues
-    #[structopt(short = "y", long, global = true)]
-    noconfirm: bool,
-}
-
-impl Options {
-    /// Getter for dotfiles directory
-    pub fn dotfiles_dir(&self) -> &PathBuf {
-        &self.dotfiles_dir
-    }
-
-    /// Getter for `noconfirm` option
-    pub fn noconfirm(&self) -> bool {
-        self.noconfirm
-    }
-
-    /// Getter for linux distro identifier
-    ///
-    /// If linux distro isn't specified by the user, it's determined using
-    /// [`crate::os::get_distro_id`]
-    pub fn distro_id(&self) -> Result<String, OSError> {
-        match &self.distro_id {
-            Some(id) => Ok(id.to_string()),
-            None => os::get_distro_id(),
-        }
-    }
 }
---
+++ src/cli/options.rs
@@ -0,0 +1,50 @@
+//! CLI options
+
+use crate::os::{self, OSError};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub struct Options {
+    /// Set a custom dotfiles directory
+    #[structopt(
+        short = "d",
+        long = "dotdir",
+        env = "DOTM_DOTFILES_DIR",
+        value_name = "DIR",
+        default_value = "./",
+        global = true
+    )]
+    dotfiles_dir: PathBuf,
+
+    /// Specify linux distribution id to use
+    #[structopt(short = "D", long = "distro", value_name = "ID", global = true)]
+    distro_id: Option<String>,
+
+    /// Use default values for confirmation dialogues
+    #[structopt(short = "y", long, global = true)]
+    noconfirm: bool,
+}
+
+impl Options {
+    /// Getter for dotfiles directory
+    pub fn dotfiles_dir(&self) -> &PathBuf {
+        &self.dotfiles_dir
+    }
+
+    /// Getter for `noconfirm` option
+    pub fn noconfirm(&self) -> bool {
+        self.noconfirm
+    }
+
+    /// Getter for linux distro identifier
+    ///
+    /// If linux distro isn't specified by the user, it's determined using
+    /// [`crate::os::get_distro_id`]
+    pub fn distro_id(&self) -> Result<String, OSError> {
+        match &self.distro_id {
+            Some(id) => Ok(id.to_string()),
+            None => os::get_distro_id(),
+        }
+    }
+}
